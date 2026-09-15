f23a6713b0af806f8e948b55836c3ee4905b554c
pierre799 <pierreguillotdel@gmail.com>
2026-09-15 16:35:02 +0200

    style grille + html

--- view/grilleEval/show.php
+++ view/grilleEval/show.php
@@ -3,56 +3,75 @@
 <?php include 'view/layout/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="public/css/grilleEval.css">
 
-<h2>Grille d'évaluation</h2>
-
-<p>Cours : <?= $modeleeval["natureGrille"]?></p>
-<p>Devoir : <?= $modeleeval["nomModuleGrilleEvaluation"]?></p>
-
-<table>
-<!--AJOUT D'UNE LIGNE POUR CHAQUE $c DANS LE TABLEAU $critereseval-->
-
-<th>Critere</th>
-    <th>Description</th>
-    <th>Note max</th>
-    <th>Note</th>
-</th>
-<?php foreach ($critereseval as $c): ?>
-    <tr>
-        <td><?= $c['descCourte'] ?></td>
-        <td><?= $c['descLongue'] ?></td>
-        <td><span id="max-value-<?= $c['IdCritere'] ?>"></span></td>
-        <td><input type="range" id="slider-<?= $c['IdCritere'] ?>" value="0" min="0" max="<?= $c["noteCritere"] ?>" step='0.25' /><br><span id="value-<?= $c['IdCritere'] ?>"></td>
-    </tr>
-<?php endforeach; ?>
-</table>
-
-<br> <!-- JE METTRAIS EN DISPLAY FLEX APRES -->
-
-<label for="">Évaluateur attribué</label>
-<select name="" id="">
-</select>
-
-<br> <!-- JE METTRAIS EN DISPLAY FLEX APRES -->
-
-<script>
-    <?php foreach ($critereseval as $c): ?>
-        var slider<?= $c['IdCritere'] ?> = document.getElementById("slider-<?= $c['IdCritere'] ?>");
-        var output<?= $c['IdCritere'] ?> = document.getElementById("value-<?= $c['IdCritere'] ?>");
-        var maxValue<?= $c['IdCritere'] ?> = document.getElementById("max-value-<?= $c['IdCritere'] ?>");
-
-        maxValue<?= $c['IdCritere'] ?>.innerHTML = <?= $c['noteCritere']?>;
-        output<?= $c['IdCritere'] ?>.innerHTML = slider<?= $c['IdCritere'] ?>.value;
-        slider<?= $c['IdCritere'] ?>.oninput = function() {
-            output<?= $c['IdCritere'] ?>.innerHTML = this.value;
-        };
-    <?php endforeach; ?>
-</script>
-
-<div>
-    <textarea><?=$feedback["commentaireJury"]?></textarea>
+<div class="info-bar">
+    <div>
+        <div>
+            <p>Cours : <?= $modeleeval["natureGrille"]?></p>
+            <p>Devoir : <?= $modeleeval["nomModuleGrilleEvaluation"]?></p>
+        </div>
+        <div>
+            <p>Nom de l'élève</p>
+        </div>
+    </div>
 </div>
 
-<div>
+<div class="content">
+    <div class="table-box">
+        <h2>Notes :</h2>
+        <table>
+
+        <th>Critere</th>
+            <th>Description</th>
+            <th>Note</th>
+        </th>
+        <?php foreach ($critereseval as $c): ?>
+            <tr>
+                <td><?= $c['descCourte'] ?></td>
+                <td><?= $c['descLongue'] ?></td>
+                <td class="slider-box">
+                    <div>    
+                        <input type="range" id="slider-<?= $c['IdCritere'] ?>" value="0" min="0" max="<?= $c["noteCritere"] ?>" step='0.25' />
+                    </div>
+                    <div>
+                        <span id="value-<?= $c['IdCritere'] ?>"></span>/<span class="max-value" id="max-value-<?= $c['IdCritere'] ?>"></span></td>
+                    </div>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    </div>
+
+    <label for="">Évaluateur attribué</label>
+    <select name="" id="">
+        <option>ezfekoizekfzioekfoizefk</option>
+        <option>ezfekoizekfzioekfoizefk</option>
+        <option>ezfekoizekfzioekfoizefk</option>
+        <option>ezfekoizekfzioekfoizefk</option>
+    </select>
+
+    <label for="">Note calculée</label>
+    <label for="" class>0.0 / 0.0</label>
+
+    <script>
+        <?php foreach ($critereseval as $c): ?>
+            var slider<?= $c['IdCritere'] ?> = document.getElementById("slider-<?= $c['IdCritere'] ?>");
+            var output<?= $c['IdCritere'] ?> = document.getElementById("value-<?= $c['IdCritere'] ?>");
+            var maxValue<?= $c['IdCritere'] ?> = document.getElementById("max-value-<?= $c['IdCritere'] ?>");
+
+            maxValue<?= $c['IdCritere'] ?>.innerHTML = <?= $c['noteCritere']?>;
+            output<?= $c['IdCritere'] ?>.innerHTML = slider<?= $c['IdCritere'] ?>.value;
+            slider<?= $c['IdCritere'] ?>.oninput = function() {
+                output<?= $c['IdCritere'] ?>.innerHTML = this.value;
+            };
+        <?php endforeach; ?>
+    </script>
+
+    <div class="feedback-box">
+        <h2>Feedback :</h2>
+        <textarea><?=$feedback["commentaireJury"]?></textarea>
+    </div>
+</div>
+
+<div class="save-bar">
     <button>Enregistrer</button>
     <button>Valider</button>
 </div>
